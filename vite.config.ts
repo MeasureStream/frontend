cdb1c19e899f732955060f0419c64d1e5244e365
R3M1d4Cod3r <superchristian9@gmail.com>
2025-03-03 12:51:10 +0100

    feat: DockerfileKeycloak and mongod.conf

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     watch: {
       usePolling: true, // Usa polling per file system watcher (utile in ambienti con file system virtuale)
     },
+    base:"/ui",
     host: "0.0.0.0",
     hmr: true, // Abilita Hot Module Replacement
   }
