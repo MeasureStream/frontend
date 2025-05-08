b86ba7b888232118d4997ccf8a651084327d98ee
christian <superchristian9@gmail.com>
2025-05-08 14:07:23 +0200

    feat: links only when logged

--- src/components/MyNavbar.tsx
+++ src/components/MyNavbar.tsx
@@ -16,18 +16,26 @@ function MyNavbar( {me} : NavbarProps ) {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" role="navigation" >
                     <Nav className="me-auto">
+
+                            <>
                         {
-                            //<Nav.Link  as = {Link} to="/">Home</Nav.Link>}
+                            me.name ?
+                                <>
+                                <Nav.Link  as = {Link} to="/measures" >Measures</Nav.Link>
+                                <Nav.Link  as = {Link} to="/dcc" >DCC</Nav.Link>
+                                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item  as = {Link} to="/add" >Add</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4"> Separated link </NavDropdown.Item>
+                                </NavDropdown>
+                                </>
+                                :
+                                <></>
+
                         }
-                    <Nav.Link  as = {Link} to="/measures" >Measures</Nav.Link>
-                        <Nav.Link  as = {Link} to="/dcc" >DCC</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item  as = {Link} to="/add" >Add</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4"> Separated link </NavDropdown.Item>
-                    </NavDropdown>
+                        </>
                     </Nav>
 
                 </Navbar.Collapse>
